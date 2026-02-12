846650a16758abded99d005073dd4a9401841648
codythai <thaiv@farmingdale.edu>
2026-02-12 00:08:15 -0500

    loginpage

---
+++ src/main/java/codythai92/bcs430loginpage/Launcher.java
@@ -0,0 +1,9 @@
+package codythai92.bcs430loginpage;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module codythai92.bcs430loginpage {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens codythai92.bcs430loginpage to javafx.fxml;
+    exports codythai92.bcs430loginpage;
+}
