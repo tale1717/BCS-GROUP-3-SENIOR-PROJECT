aa8e9d1a1c53ffe3ba8ad10aa25a223b031b4872
Jared Tobias <jaredtobias12@gmail.com>
2026-02-20 22:46:42 -0500

    Connected Customer Register/Sign-In pages to Firebase and updated Employee sign in page

--- BarberShopWebsite/customer-form.js
+++ BarberShopWebsite/customer-form.js
@@ -1,4 +1,4 @@
-  import { firebaseConfig } from "./firebaseConfig.js";
+  import { firebaseConfig } from "../firebaseConfig.js";
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
   import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
@@ -7,31 +7,16 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-document.getElementById("reg-button").addEventListener('click', function() {
-    document.getElementById("register-div").style.display = "inline";
-    document.getElementById("login-div").style.display = "none";
-
-});
-
-document.getElementById("loginpage").addEventListener('click', function() {
-    document.getElementById("register-div").style.display = "none";
-    document.getElementById("login-div").style.display = "inline";
-
-});
-
-//  login form submission
-document.getElementById("submit").addEventListener('click', function() {
-    const email = document.getElementById("signin-username").value;
+document.getElementById("signin-form").addEventListener('submit', function(e) {
+    e.preventDefault();
+    const email = document.getElementById("signin-email").value;
     const password = document.getElementById("signin-password").value;
 
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
            
             const user = userCredential.user;
-            window.location.href = "BarberShopManagement/index.html";
-            // document.getElementById("result-box").style.display = "inline";
-            // document.getElementById("login-div").style.display = "none";
-            // document.getElementById("result").innerHTML = "Login successful! Welcome to Triple T and G Barbershop ";
+            alert("Login successful! Welcome to Triple T & G Barbershop");
           
         })
         .catch((error) => {
@@ -43,16 +28,20 @@ document.getElementById("submit").addEventListener('click', function() {
 });
 
 //signup form submission
-document.getElementById("register").addEventListener('click', function() {
-    const email = document.getElementById("reg-email").value;
-    const password = document.getElementById("reg-password").value;
+document.getElementById("register-form").addEventListener('submit', function(e) {
+    e.preventDefault();
+    const FirstName = document.getElementById("first-name").value;
+    const LastName = document.getElementById("last-name").value;
+    const email = document.getElementById("customer-email").value;
+    const dob = document.getElementById("customer-dob").value;
+    const password = document.getElementById("customer-password").value;
 
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             const user = userCredential.user;
             alert("Registration successful! You can now log in.");
-            document.getElementById("register-div").style.display = "none";
-            document.getElementById("login-div").style.display = "inline";
+            // document.getElementById("register-div").style.display = "none";
+            // document.getElementById("login-div").style.display = "inline";
         })
         .catch((error) => {
             const errorCode = error.code;
