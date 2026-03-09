7176cfebd4df8b4f3858be2f87ef2f9968e3d40a
Vinh Co Thai <thaiv@farmingdale.edu>
2026-03-08 22:07:17 -0400

    appointment module html, js and css.

--- BarberShopWebsite/Collections/appointments.js
+++ BarberShopWebsite/Collections/appointments.js
@@ -20,3 +20,31 @@ export async function getAppointment(appointmentId) {
     const snap = await getDoc(doc(db, "appointments", appointmentId));
     return snap.exists() ? { id: snap.id, ...snap.data() } : null;
 }
+
+// Get Appointment from database to appointment modules
+export async function getAppointments() {
+
+    const snap = await getDocs(collection(db, "appointments"));
+
+    return snap.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+
+}
+
+
+// update Appointment  from appointment modules to firebase
+export async function updateAppointment(id, data) {
+
+    await updateDoc(doc(db, "appointments", id), data);
+
+}
+
+
+// Delete appointment from appointment modules
+export async function deleteAppointment(id) {
+
+    await deleteDoc(doc(db, "appointments", id));
+
+}
